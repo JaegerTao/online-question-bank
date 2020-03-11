700299754ff0dbe02b52db7565c3a8bf934c05bd
Jaeger <46061258+JaegerTao@users.noreply.github.com>
2020-03-11 22:28:12 +0800

    建立项目框架
    使用uni-app开发，添加了colorUI

---
+++ pages/exam/exam.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="">
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="content">
+		<button type="primary" @tap="goTest">点击考试</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			goTest(){
+				uni.navigateTo({
+					url: './exam/exam',
+					success: res => {},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+</style>
